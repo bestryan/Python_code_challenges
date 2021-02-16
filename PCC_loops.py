349a36008e3b32ad841f8a26d3f2c5c31375d7e5
bestryan88@gmail.com <bestryan88@gmail.com>
2021-02-16 23:26:33 +1100

    second script update

--- PCC_loops.py
+++ PCC_loops.py
@@ -10,3 +10,17 @@ def divisible_by_ten(nums):
      
 #Uncomment the line below when your function is done
 print(divisible_by_ten([20, 25, 30, 35, 40]))
+
+
+# Greetings
+
+#Write your function here
+def add_greetings(names):
+  new_greetings = []
+  for i in names:
+    new_greetings.append("Hello, " + i)
+  return new_greetings
+
+#Uncomment the line below when your function is done
+print(add_greetings(["Owen", "Max", "Sophie"]))
+
