88e09fe09adaa67afe03ab1672cee81632e05041
bestryan88@gmail.com <bestryan88@gmail.com>
2021-02-18 23:19:38 +1100

    question 3, 4 code add

--- PCC_loops.py
+++ PCC_loops.py
@@ -1,5 +1,4 @@
 # Divisible by Ten
-
 #Write your function here
 def divisible_by_ten(nums):
   count = 0
@@ -13,7 +12,6 @@ print(divisible_by_ten([20, 25, 30, 35, 40]))
 
 
 # Greetings
-
 #Write your function here
 def add_greetings(names):
   new_greetings = []
@@ -24,3 +22,26 @@ def add_greetings(names):
 #Uncomment the line below when your function is done
 print(add_greetings(["Owen", "Max", "Sophie"]))
 
+
+# Delete Starting Even Numbers
+#Write your function here
+def delete_starting_evens(lst):
+  while len(lst) > 0 and lst[0] % 2 == 0:
+    lst.pop(0)
+  return lst
+
+#Uncomment the lines below when your function is done
+print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
+print(delete_starting_evens([4, 8, 10]))
+
+
+
+# Odd Indices
+#Write your function here
+def odd_indices(lst):
+  new_list = []
+  for index in range(1, len(lst), 2):
+    new_list.append(lst[index]) 
+  return new_list
+#Uncomment the line below when your function is done
+print(odd_indices([4, 3, 7, 10, 11, -2]))
